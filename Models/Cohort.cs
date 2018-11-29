6f346d3f5541bdae44e5f847aca57b89d0abc399
gretchenmarie <gretchenmward@gmail.com>
2018-11-29 14:21:20 -0600

    cohorts list

---
+++ Models/Cohort.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace StudentExercisesAPI.Data
+{
+    public class Cohort
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+
+}
---
+++ Models/View Models/CohortCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExerciseMVC.Models.ViewModels
+    {
+        public class CohortCreateViewModel
+        {
+            private readonly IConfiguration _config;
+
+        
+            public Cohort cohort { get; set; }
+
+            public CohortCreateViewModel() { }
+
+           
+        }
+    }
+
